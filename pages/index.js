9061e34da841ea4547045dc8f5c163288b7eab80
Navi Singh <navisingh@live.in>
2020-11-07 01:02:44 +0530

    Added basic elements of the page

--- pages/index.js
+++ pages/index.js
@@ -14,10 +14,15 @@ export default function Home() {
 			</Head>
 			<Header>
 				<div className='logo' />
-				<Menu theme='dark' mode='horizontal' defaultSelectedKeys={['1']}>
-					<Menu.Item key='1'>nav 1</Menu.Item>
-					<Menu.Item key='2'>nav 2</Menu.Item>
-					<Menu.Item key='3'>nav 3</Menu.Item>
+				<Menu theme='light' mode='horizontal' defaultSelectedKeys={['home']}>
+					<Menu.Item key='home'>Home</Menu.Item>
+					<Menu.Item key='appointment'>Schedule Appointment</Menu.Item>
+					<Menu.Item key='services'>Our Services</Menu.Item>
+					<Menu.Item key='faq'>FAQ</Menu.Item>
+					<Menu.Item key='careers'>Careers</Menu.Item>
+					<Menu.Item key='contact'>Contact Us</Menu.Item>
+					<Menu.Item key='claim'>File a Claim</Menu.Item>
+					<Menu.Item key='about'>About Us</Menu.Item>
 				</Menu>
 			</Header>
 			<Content style={{ padding: '0 50px' }}>
