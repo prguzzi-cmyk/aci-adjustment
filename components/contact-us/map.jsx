fd215b0794d0eb95c120b65fc1e262003c51368e
Navi Singh <navisingh@live.in>
2021-04-19 21:44:14 +0530

    Updated map

--- components/contact-us/map.jsx
+++ components/contact-us/map.jsx
@@ -2,7 +2,7 @@ const MapSection = () => {
 	return (
 		<div className='map-container'>
 			<iframe
-				src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1523.085537126385!2d-75.06403984644852!3d40.22749838327828!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c402b9312f8403%3A0xfc95f0c5fc25e345!2sACI%20Adjustment%20Group!5e0!3m2!1sen!2sin!4v1607975515323!5m2!1sen!2sin'
+				src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3043.759012494612!2d-74.99644098510991!3d40.281103171801675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c6aa944c159643%3A0xe74b63b5592535d3!2s803%20Park%20Ave%2C%20Newtown%2C%20PA%2018940!5e0!3m2!1sen!2sus!4v1618848135106!5m2!1sen!2sus'
 				width='100%'
 				height='450'
 				frameBorder='0'
